22f8ffc34fb11a4b97aed5c22e203e97d0d6d6bb
Marco de Lannoy Kobayashi <marcodlk@hawaii.edu>
2018-10-19 16:42:49 -1000

    Update tests/test_base.py

--- dash_building_blocks/tests/test_base.py
+++ dash_building_blocks/tests/test_base.py
@@ -4,10 +4,9 @@ from unittest import mock
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 from dash_building_blocks.base import (
-    Block, Store
+    Block, Store, Data, Component
 )
 from dash_building_blocks.error import (
-    Error,
     ProhibitedParameterError
 )
 
@@ -29,6 +28,77 @@ class HelloWorld(Block):
         def update_div(value):
             return value
 
+class TestData(unittest.TestCase):
+
+    def setUp(self):
+        self.kwargs = {
+            'str': 'hello',
+            'int': 10,
+            'float': 1.5,
+            'dict': {'answer': 42},
+            'list': [0, 1, 2, 3]
+        }
+
+    def test_init_default(self):
+        data = Data()
+        self.assertEqual(data.__dict__, {})
+        self.assertFalse(data)
+
+    def test_death_init_args(self):
+        try:
+            Data('kill me')
+        except:
+            self.assertRaises(TypeError)
+        
+    def test_init_kwargs(self):
+        data = Data(**self.kwargs)
+        self.assertTrue(data)
+
+        for key, val in self.kwargs.items():
+            self.assertEqual(getattr(data, key), val)
+            self.assertEqual(data[key], val)
+
+        self.assertEqual(repr(self.kwargs), repr(data))
+        self.assertEqual(str(self.kwargs), str(data))
+
+    def test_from_dict_to_dict(self):
+        data = Data.from_dict(self.kwargs)
+        self.assertEqual(data.to_dict(), self.kwargs)
+
+
+class TestComponent(unittest.TestCase):
+
+    def setUp(self):
+        self.id = 'test-component'
+        self.comp = Component(self.id)
+        self.assertEqual(self.comp.id, self.id)
+
+    def test_death_init_default(self):
+        try:
+            Component()
+        except:
+            self.assertRaises(TypeError)
+
+    def assertEqualDependencies(self, dep1, dep2):
+        self.assertEqual(type(dep1), type(dep2))
+        self.assertEqual(dep1.component_id, dep2.component_id)
+        self.assertEqual(dep1.component_property, dep2.component_property)
+
+    def test_getitem(self):
+        self.assertEqual(self.comp['prop'], (self.id, 'prop'))
+
+    def test_input(self):
+        self.assertEqualDependencies(
+            self.comp.input('prop'), Input(self.id, 'prop'))
+
+    def test_state(self):
+        self.assertEqualDependencies(
+            self.comp.state('prop'), State(self.id, 'prop'))
+
+    def test_output(self):
+        self.assertEqualDependencies(
+            self.comp.output('prop'), Output(self.id, 'prop'))
+
 
 class TestBaseBlock(unittest.TestCase):
 
@@ -37,6 +107,7 @@ class TestBaseBlock(unittest.TestCase):
             Block()
         except:
             self.assertRaises(NotImplementedError)
+
 
 class TestBlockMinimal(unittest.TestCase):
 
@@ -94,7 +165,7 @@ class TestBlockMinimal(unittest.TestCase):
         for param in Block.sacred_attrs:
             try:
                 Minimal().parameters(**{param:'killme'})
-            except Error:
+            except:
                 self.assertRaises(ProhibitedParameterError)
 
 
