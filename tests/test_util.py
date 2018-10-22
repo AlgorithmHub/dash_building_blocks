836cef8abb050400c071e43ce7e64ae6492150b6
Marco de Lannoy Kobayashi <marcodlk@hawaii.edu>
2018-10-22 13:00:34 -1000

    Migrate tests directory to project top-level directory

---
+++ tests/test_util.py
@@ -0,0 +1,44 @@
+import unittest
+
+from dash_building_blocks.util import (
+    camelify,
+    decamelify
+)
+
+class TestCamelify(unittest.TestCase):
+
+    def test_camelify_default_delims(self):
+        origs = ['this-is-a-test', '_this-is_a-test_']
+        camels = ['thisIsATest', 'ThisIsATest']
+        
+        for orig, camel in zip(origs, camels):
+            self.assertEqual(camelify(orig), camel)
+
+    def test_camelify_custom_delims(self):
+        origs = ['this*is*a*test', 'thisaisatest']
+        camels = ['thisIsATest', 'thisIsTest']
+        delims = [['*'], ['a']]
+        
+        for orig, camel, delim in zip(origs, camels, delims):
+            self.assertEqual(camelify(orig, delims=delim), camel)
+
+class TestDeCamelify(unittest.TestCase):
+
+    def test_decamelify_default_delim(self):
+        camels = ['thisIsATest', 'ThisIsATest']
+        decamels = ['this-is-a-test', 'this-is-a-test']
+        
+        for camel, decam in zip(camels, decamels):
+            self.assertEqual(decamelify(camel), decam)
+
+    def test_decamelify_custom_delim(self):
+        camels = ['thisIsATest', 'ThisIsTest']
+        decamels = ['this*is*a*test', 'thisaisatest']
+        delims = ['*', 'a']
+        
+        for camel, decam, delim in zip(camels, decamels, delims):
+            self.assertEqual(decamelify(camel, delim=delim), decam)
+
+
+if __name__ == '__main__':
+    unittest.main()
